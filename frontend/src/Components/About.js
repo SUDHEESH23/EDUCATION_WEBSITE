3ecec157ba079766eb808a4c0f675d78a875476f
Sudheesh Patwardhan <sudheesh.is21@gmail.com>
2023-07-27 12:20:10 +0530

    updated

---
+++ frontend/src/Components/About.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>
+      <h2>Delicious Eats</h2>
+
+<p>Welcome to Delicious Eats, your ultimate destination for culinary delights and memorable dining experiences! Founded in 2023, Delicious Eats is a leading food company that aims to tantalize your taste buds and make every meal a delightful adventure.</p>
+
+
+
+<p>At Delicious Eats, our mission is simple yet powerful – to create exceptional and diverse dining experiences that cater to every palate. We are committed to offering the finest quality of food, prepared with love and passion by our team of talented chefs. Whether you're craving traditional favorites or exploring unique flavors from around the world, we have something to satisfy every craving.</p>
+
+
+
+
+      </p>
+      <h2>Contact Information</h2>
+      <ul>
+        <li>Address: 123 Main Street, City, State, ZIP</li>
+        <li>Phone: (555) 123-4567</li>
+        <li>Email: info@delicious-eats.com</li>
+        <li>Website: www.delicious-eats.com</li>
+      </ul>
+    </div>
+  );
+};
+
+export default About;
